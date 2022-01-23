dfa5b0232c2d834f0665cce4eedc5aad354f5ed1
Abhishek Tiwari <abhishek.tiwari17338@marwadieducation.edu.in>
2022-01-23 22:02:41 +0530

    modification in js file, css file, html file and add home file

--- Registration.js
+++ Registration.js
@@ -1,32 +1,87 @@
-const inputField=document.querySelectorAll('input');
-const submit = document.querySelector('#submit');
-const errorMsg = document.querySelector(".error");
-const myForm = document.getElementById('myFrm');
+const input=document.querySelectorAll('input');
+const myForm = document.getElementById('myForm');
 
-inputField.forEach((el)=>{
+input.forEach((el)=>{
   el.addEventListener('blur',(event)=>{
     event.preventDefault();
 
+    let showErr = el.nextElementSibling;
+
+    console.log(showErr);
+
     if(el.value.trim()==''){
-      error(el,'field can not be empty');
+      blurError(el,showErr);
     }
 
+    else if(el.type == 'submit'){
+      el.style.border='none';
+    }
     
+    else if(el.type == 'email'){
+      if(checkEmail(el.value)){
+        success(el,showErr);
+      }
+      else notValid(el,showErr);
+    }
+
+    else if(el.type=='number'){      
+    if(checkNumber(el.value)){  
+        success(el,showErr);      
+      }                            
+      else notValid(el,showErr);  
+    }                             
+
     else {
-      success(el);
+      success(el,showErr);
     }
 
   });
 });
 
+// inputField.forEach((el)=>{
+//   el.addEventListener('submit',(event)=>{
+//     event.preventDefault();
 
-const error = (element,msg)=>{
+//     alert('form not submite');
+
+//     let submitErr = el.nextElementSibling;
+
+//     if(el.value.trim()==''){
+//       blurError(el,submitErr);
+//     }
+
+//   });
+// });
+
+function notValid(element, showErr) {
+  element.style.border = '3px red solid';
+  showErr.innerHTML = '*you must enter valid ' + element.name;
+}
+
+const blurError = (element,showErr)=>{
   element.style.border='3px red solid';
-   errorMsg.innerHTML=msg;
+  showErr.innerHTML='*please enter '+element.name;
 }
 
 
-const success = (element)=>{
-  element.style.border='3px green solid';
-  errorMsg.innerHTML="";
+const success = (element,showErr)=>{
+  element.style.border='';
+  element.style.borderRight='5px green solid';
+  element.style.borderBottom='3px green solid';
+  showErr.innerHTML="";
 }
+
+
+const checkEmail = (email) => {
+	return email.match(
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	);
+}
+
+const checkNumber = (num) => {
+	return num.match(/^[0-9]{10}$/);
+}
+
+// const checkPassword = (password) => {
+// 	return password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm);
+// }
