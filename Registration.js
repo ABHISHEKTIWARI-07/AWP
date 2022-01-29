db223ce8ab3a46f3d2bab1785b72a67d6d443392
Abhishek Tiwari <abhishek.tiwari17338@marwadieducation.edu.in>
2022-01-29 13:54:11 +0530

    Register.js: modufied blur event and submit event

--- Registration.js
+++ Registration.js
@@ -29,29 +29,113 @@ input.forEach((el)=>{
         success(el,showErr);      
       }                            
       else notValid(el,showErr);  
-    }                             
+    }
+    
+    else if(el.type=='password'){
+      if(checkPassword(el.value)){
+        success(el,showErr);
+      }
+      else{
+        showErr.innerHTML = '*enter minimum 8 character password';
+      }
+    }
 
     else {
       success(el,showErr);
     }
 
+    verifyFormField(event.target);
+
   });
 });
 
-// inputField.forEach((el)=>{
-//   el.addEventListener('submit',(event)=>{
-//     event.preventDefault();
 
-//     alert('form not submite');
 
-//     let submitErr = el.nextElementSibling;
+myForm.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  // confirm("Have you filled the details");
 
-//     if(el.value.trim()==''){
-//       blurError(el,submitErr);
-//     }
+  if (!verifyAllFields()) {
+    alert("successfully Registered...");
+    document.myForm.reset();
+	} else {
+		let invalidInputs = document.getElementById('invalid');
+    invalidInputs.innerHTML = "Please fill all the inputs...";
+    // invalidInputs.style.backgroundColor = "red";
+    invalidInputs.style.color = "red";
+	}
 
-//   });
-// });
+})
+
+const verifyAllFields = ()=>{
+    let flag = true;
+  input.forEach((element) => {
+  	if(!verifyFormField(element)){
+      flag = false;
+    } 
+    // else flag = true;
+	});
+  return flag;
+
+}
+
+const verifyFormField = (el) => {
+	console.log(el);
+	let formFlag = true;
+  let showErr = document.getElementsByTagName('span');
+
+
+  if(el.value.trim()==''){
+    blurError(el,showErr);
+    flag = false;
+  }
+
+  else if(el.type=='submit'){
+    el.style.border='none';
+  }
+
+   else if(el.type == 'email'){
+      if(checkEmail(el.value)){
+        success(el,showErr);
+        formFlag = true;
+      }
+      else {
+        notValid(el,showErr);
+        formFlag=false
+      }
+    }
+
+    else if(el.type=='number'){      
+    if(checkNumber(el.value)){  
+        success(el,showErr); 
+        formFlag=false;     
+      }                            
+      else {
+        notValid(el,showErr);
+        formFlag=false;
+      }  
+    }
+    
+    else if(el.type=='password'){
+      if(checkPassword(el.value)){
+        success(el,showErr);
+        formFlag=true;
+      }
+      else{
+        showErr.innerHTML = '*enter minimum 8 character password';
+        formFlag=false;
+      }
+    }
+
+    else {
+      success(el,showErr);
+      formFlag=true;
+    }
+
+	return formFlag;
+}
+
+
 
 function notValid(element, showErr) {
   element.style.border = '3px red solid';
@@ -82,6 +166,6 @@ const checkNumber = (num) => {
 	return num.match(/^[0-9]{10}$/);
 }
 
-// const checkPassword = (password) => {
-// 	return password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm);
-// }
+const checkPassword = (password) => {
+	return password.match(/^[A-Za-z\-0-9]{8,16}$/g);
+}
